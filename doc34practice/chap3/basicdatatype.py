87696729d554da1488f8a33089d8b41ed7809193
bjzhush <bjzhush@gmail.com>
2016-10-19 22:41:14 +0800

    add code

---
+++ doc34practice/chap3/basicdatatype.py
@@ -0,0 +1,26 @@
+print("number:")
+print(2+4)
+print("string:")
+print('hello single quotes')
+print("single ' in double quotes")
+print("slash double quotes \"hi\" in double quotes")
+print("""
+    第一行
+    跨行内容用三个\"开始和结束
+    第三行
+""")
+
+print("字符和变量连接用+")
+prefix = "Py"
+print(prefix + "thon")
+
+word = 'hello world'
+print("操作hello world")
+print("用[0]取字符串第一个字符")
+print(word[0])
+print("用[0:2]取字符串字符")
+print(word[0:2])
+print("用[:3]取字符串字符")
+print(word[:3])
+print("用[-2:]取字符串字符")
+print(word[-2:])
---
+++ doc34practice/chap3/basicdatatypelist.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+week = ['m','t','w','t','f','s','s']
+print(week)
+print(week[2])
+print("list只能拼list")
+week = week + ['xingqi8']
+print(week)
+print('len 查看长度，字符和list通用')
+print(len(week))
+
